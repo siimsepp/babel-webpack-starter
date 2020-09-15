04d02a62ac598524411491c27a37d9917155f2c3
Siim Sepp <siim.sepp@gmail.com>
2020-09-15 11:29:13 +0300

    kood

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-const greeting = 'Hello World';
-console.log(greeting);
-
-const getData = async url => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
-
-getData('https://jsonplaceholder.typicode.com/posts');
+const tervitus = 'Tere, Siim!';
+document.querySelector('h3').textContent = tervitus;
+console.log(tervitus);
